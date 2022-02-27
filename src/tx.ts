5d2ed56c671ca537c0c152bdd915811d76a25d56
jojoldu <jojoldu@gmail.com>
2022-02-27 23:16:00 +0900

    코드 리팩토링

--- src/tx.ts
+++ src/tx.ts
@@ -16,14 +16,15 @@ async function rollback(client: PoolClient) {
 
 export async function insertAll(size: number, failNumber: number) {
     const client = await pool.connect();
+    const funcName = 'insertAll';
     try {
         await client.query('BEGIN');
 
         await Promise.all([
-            insert(1, failNumber, 'insertAll'),
-            insert(2, failNumber, 'insertAll'),
-            insertThrow(3, failNumber, 'insertAll'),
-            insert(4, failNumber, 'insertAll'),
+            insert(1, failNumber, funcName),
+            insert(2, failNumber, funcName),
+            insertThrow(3, failNumber, funcName),
+            insert(4, failNumber, funcName),
         ]);
 
         await commit(client);
@@ -37,15 +38,15 @@ export async function insertAll(size: number, failNumber: number) {
 
 export async function insertAllWithAllSettled(size: number, failNumber: number) {
     const client = await pool.connect();
-
+    const funName = 'insertAllWithAllSettled'
     try {
         await client.query('BEGIN');
 
         const result = await Promise.allSettled([
-            insert(1, failNumber, 'insertAll'),
-            insert(2, failNumber, 'insertAll'),
-            insertThrow(3, failNumber, 'insertAll'),
-            insert(4, failNumber, 'insertAll'),
+            insert(1, failNumber, funName),
+            insert(2, failNumber, funName),
+            insertThrow(3, failNumber, funName),
+            insert(4, failNumber, funName),
         ]);
 
         if (result.map(r => r.status === 'rejected').length > 0) {
@@ -63,11 +64,16 @@ export async function insertAllWithAllSettled(size: number, failNumber: number) 
 
 export async function insertAllWithPool(size: number, failNumber: number) {
     const client = await pool.connect();
+    const funcName = 'insertAllWithPool'
     try {
         await client.query('BEGIN');
 
-        await Promise.all(Array(size).fill(0)
-            .map((_, i) => insertWithPerformance(i + 1, failNumber, 'insertAllWithPoolAndAllSettled', client)))
+        await Promise.all([
+            insert(1, failNumber, funcName, client),
+            insert(2, failNumber, funcName, client),
+            insertThrow(3, failNumber, funcName, client),
+            insert(4, failNumber, funcName, client)
+        ]);
 
         await commit(client);
     } catch (error) {
@@ -80,12 +86,16 @@ export async function insertAllWithPool(size: number, failNumber: number) {
 
 export async function insertAllWithPoolAndAllSettled(size: number, failNumber: number) {
     const client = await pool.connect();
-
+    const funcName = 'insertAllWithPoolAndAllSettled'
     try {
         await client.query('BEGIN');
 
-        const result = await Promise.allSettled(Array(size).fill(0)
-            .map((_, i) => insertWithPerformance(i + 1, failNumber, 'insertAllWithPoolAndAllSettled', client)));
+        const result = await Promise.allSettled([
+            insert(1, failNumber, funcName, client),
+            insert(2, failNumber, funcName, client),
+            insertThrow(3, failNumber, funcName, client),
+            insert(4, failNumber, funcName, client)
+        ]);
 
         if (result.map(r => r.status === 'rejected').length > 0) {
             throw new Error('Promise.allSettled exist Error');
@@ -98,6 +108,23 @@ export async function insertAllWithPoolAndAllSettled(size: number, failNumber: n
     } finally {
         await client.release();
     }
+}
+
+export async function insert(sec: number, failNumber: number, funcName: string, client?: PoolClient) {
+    const sql = `insert into node_test (name, sleep)
+                 values (\'${funcName}-${sec}\', pg_sleep(${sec}))`;
+
+    const connection = client ? client : await pool.connect();
+    return connection.query(sql);
+}
+
+export async function insertThrow(sec: number, failNumber: number, funcName: string, client?: PoolClient) {
+    // 오류나는 쿼리
+    const sql = `insert into node_test (name, sleep, throw)
+                 values (\'${funcName}-${sec}\', pg_sleep(${sec}))`;
+
+    const connection = client ? client : await pool.connect();
+    return connection.query(sql);
 }
 
 export async function insertWithPerformance(sec: number, failNumber: number, funcName: string, client?: PoolClient) {
@@ -116,23 +143,6 @@ export async function insertWithPerformance(sec: number, failNumber: number, fun
     }
 
     return result;
-}
-
-export async function insert(sec: number, failNumber: number, funcName: string, client?: PoolClient) {
-    const sql = `insert into node_test (name, sleep)
-                 values (\'${funcName}-${sec}\', pg_sleep(${sec}))`;
-
-    const connection = client ? client : await pool.connect();
-    return connection.query(sql);
-}
-
-export async function insertThrow(sec: number, failNumber: number, funcName: string, client?: PoolClient) {
-    // 오류나는 쿼리
-    const sql = `insert into node_test (name, sleep, throw)
-                 values (\'${funcName}-${sec}\', pg_sleep(${sec}))`;
-
-    const connection = client ? client : await pool.connect();
-    return connection.query(sql);
 }
 
 export async function selectAll(): Promise<INodeTest[]> {
